8c2ae4bdd2c105fff8b10312869835e6f06bdb78
ling <ling@ling-PC.mshome.net>
2015-09-07 19:52:34 +0800

    项目提交

---
+++ src/test.java
@@ -0,0 +1,16 @@
+
+public class test {
+      int n,m;
+      public void bijiao(int n,int m){
+      if(n<m){
+    	  System.out.print("n小于m");
+      }
+      if(n==m){
+    	 System.out.print("n等于m");
+      }
+      if(n>m){
+    	  System.out.print("n大于m"); 
+      }
+    }
+}
+
